fd6ac71b7bc17d4940c062d1b0ff192719e72248
widmogrod <widmogrod@gmail.com>
2014-12-23 21:18:15 +0100

    Update path to requirejs.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+require.config({
+    name: 'main',
+    paths: {
+        async: '../vendor/requirejs-plugins/src/async',
+        text: '../vendor/requirejs-text/text',
+        jquery: '../vendor/jquery/src/jquery',
+        moment: '../vendor/moment/moment',
+        momentLocale: '../vendor/moment/locale',
+        jef: '../vendor/jef/src'
+    }
+});
+
 define(function () {
     return {};
 });

e9d6b8f88f97e928072df39d8a0e7195b13dc4d5
widmogrod <widmogrod@gmail.com>
2014-12-26 18:10:18 +0100

    Look and fill

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,13 @@
 require.config({
     name: 'main',
     paths: {
-        async: '../vendor/requirejs-plugins/src/async',
         text: '../vendor/requirejs-text/text',
-        jquery: '../vendor/jquery/src/jquery',
-        moment: '../vendor/moment/moment',
-        momentLocale: '../vendor/moment/locale',
         jef: '../vendor/jef/src'
     }
 });
 
 define(function () {
-    return {};
+    'use strict';
+
+
 });
---
+++ public/js/stream.js
@@ -0,0 +1,115 @@
+define([
+    'jef/stream/stream',
+    'jef/stream/map',
+    'jef/stream/filter',
+    'jef/stream/reduce',
+    'jef/stream/concat',
+    'jef/stream/group',
+    'jef/stream/merge',
+    'jef/stream/take',
+    'jef/stream/skip',
+    'jef/stream/both',
+    'jef/stream/distinct',
+    'jef/stream/debounce',
+    'jef/stream/when',
+    'jef/stream/noop',
+    'jef/stream/log',
+    'jef/stream/last',
+    'jef/stream/timeout',
+    'jef/stream/fromArray',
+    'jef/stream/fromEmitter',
+    'jef/stream/fromCallback',
+    'jef/stream/fromPromise',
+    'jef/stream/toArray',
+    'jef/stream/push-stream',
+    'jef/stream/push-consume'
+], function(
+    Stream,
+    map,
+    filter,
+    reduce,
+    concat,
+    group,
+    merge,
+    take,
+    skip,
+    both,
+    distinct,
+    debounce,
+    when,
+    noop,
+    log,
+    last,
+    timeout,
+    fromArray,
+    fromEmitter,
+    fromCallback,
+    fromPromise,
+    toArray,
+    PushStream,
+    consume
+) {
+    'use strict';
+
+    Stream.prototype.map = function(fn) {
+        return map(this, fn);
+    };
+    Stream.prototype.filter = function(fn) {
+        return filter(this, fn);
+    };
+    Stream.prototype.reduce = function(fn, base) {
+        return reduce(this, fn, base);
+    };
+    Stream.prototype.concat = function() {
+        return concat(this);
+    };
+    Stream.prototype.group = function(fn) {
+        return group(this, fn);
+    };
+    Stream.prototype.merge = function(stream) {
+        return merge(this, stream);
+    };
+    Stream.prototype.take = function(n) {
+        return take(this, n);
+    };
+    Stream.prototype.skip = function(n) {
+        return skip(this, n);
+    };
+    Stream.prototype.distinct = function() {
+        return distinct(this);
+    };
+    Stream.prototype.debounce = function(timeout) {
+        return debounce(this, timeout);
+    };
+    Stream.prototype.timeout = function(wait) {
+        return timeout(this, wait | 0);
+    };
+    Stream.prototype.log = function(namespace) {
+        return log(this, namespace || '');
+    };
+    Stream.prototype.last = function() {
+        return last(this);
+    };
+    Stream.prototype.toArray = function() {
+        return toArray(this);
+    };
+
+    // Factories
+    Stream.fromArray = fromArray;
+    Stream.fromEmitter = fromEmitter;
+    Stream.fromCallback = fromCallback;
+    Stream.fromPromise = fromPromise;
+    Stream.both = both;
+    Stream.when = when;
+    Stream.noop = noop;
+    Stream.merge = merge;
+    Stream.timeout = timeout;
+
+    // New classes
+    Stream.Push = PushStream;
+    Stream.Push.prototype.consume = function(stream) {
+        return consume(this, stream);
+    };
+
+    return Stream;
+});

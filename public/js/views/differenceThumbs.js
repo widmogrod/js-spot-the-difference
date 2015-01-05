f091c71f6dd4b1e51225f4e02efb717e92bb508b
widmogrod <widmogrod@gmail.com>
2015-01-05 18:40:18 +0100

    Add remove button

--- public/js/views/differenceThumbs.js
+++ public/js/views/differenceThumbs.js
@@ -5,13 +5,18 @@ define(['../dom/el'], function (el) {
         return diffs.map(function (diff) {
             return el('div', {class: 'difference tile'}, [
                 el('canvas', {
-                    class: 'fit',
+                    'class': 'fit',
                     'data-id': diff.id,
                     // This is only to patch domDiff updating given properties
                     // and therefore clearing the canvas
-                    width: diff.thumb.width,
-                    height: diff.thumb.height
-                })
+                    'width': diff.thumb.width,
+                    'height': diff.thumb.height
+                }),
+                el('span', {
+                    'class': 'action overlay',
+                    'data-action': 'remove-diff',
+                    'data-id': diff.id
+                }, '-')
             ]);
         });
     };

be5fe22e2e0bdbf3ba12395ff9f440ee5d7f843e
widmogrod <widmogrod@gmail.com>
2015-01-06 17:09:47 +0100

    Fix floating point problem

--- public/js/dom/position.js
+++ public/js/dom/position.js
@@ -1,13 +1,15 @@
 define(function() {
     'use strict';
 
+    var int = parseInt;
+
     return function position(element) {
         var x = 0;
         var y = 0;
 
         while (element) {
-            x += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-            y += (element.offsetTop - element.scrollTop + element.clientTop);
+            x += (int(element.offsetLeft) - int(element.scrollLeft) + int(element.clientLeft >> 0));
+            y += (int(element.offsetTop) - int(element.scrollTop) + int(element.clientTop >> 0));
             element = element.offsetParent;
         }
 
